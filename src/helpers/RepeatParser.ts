bfe52f03b7a572b8d97cdae1046f5b7625500d80
Richard Domingo <chardskarth@gmail.com>
2017-03-11 16:11:20 +0800

    RepeatParser now working

--- src/helpers/RepeatParser.ts
+++ src/helpers/RepeatParser.ts
@@ -8,6 +8,11 @@ export function parse(input) {
       this._initial = i;
       return this;
     }
+    , useDefaultSpace(){
+      this._defaultSpaceRegex = /\s+/i;
+      this._isUsingDefaultSpace = true;
+      return this;
+    }
     , addEvent(name: String, from: String  & String[], regex: RegExp, to?: String) {
       to = to || name;
 
@@ -15,17 +20,28 @@ export function parse(input) {
       this._events.push({name, from, to, regex});
       return this;
     }
+    , finalStates(...states) {
+      this._finalStates = states;
+      return this;
+    }
     , create() {
       var create = this._create = {};
       var initial = this._initial;
       var events = this._events;
+      var self = this;
       initial && (create["initial"] = initial);
       create["events"] = this._events.map(x => pick(x, 'name', 'from', 'to'));
       var fs = (this._fs = StateMachine.create(create));
 
       var lastToken; //for debugging
       var lexer = this._lexer = new Lexer(function (token) {
-        throw new Error(`Unexpected "${token}" near "${lastToken}"`);
+
+        //if space is enabled, do nothing
+        if(self._isUsingDefaultSpace && self._defaultSpaceRegex.test(token)){}
+        else {
+          throw new Error(`Unexpected "${token}" near "${lastToken}"`);
+
+        }
       });
       this._events.map(x => pick(x, 'name', 'regex'))
         .forEach(function (evt) {
@@ -34,26 +50,40 @@ export function parse(input) {
             fs[evt.name]();
           });
         });
-        
+      
+      var originalLex = lexer.lex;
+      lexer.lex = function(){
+        var retVal = originalLex.apply(lexer, arguments);
+        console.log(fs.current);
+        var isInFinalStates = self._finalStates.find(x => x == fs.current);
+        if(!isInFinalStates) throw Error("Not in final state!");
+        return retVal;
+      }
       return {fs, lexer};
     }
   });
   var {fs, lexer} = stateMachine.initial("none")
+    .useDefaultSpace()
 // go to "every" state, only when from state "none". input should be the regex
     .addEvent("every", "none", /every/i)
     .addEvent("dayset", "every", /day/i)
     .addEvent("weekdayset", "every", /((monday)|(tuesday)|(wednesday)|(thursday)|(friday)|(sunday)|(saturday))/i)
     .addEvent("weekdayadding", "weekdayset", /(and)|(,)/i)
     .addEvent("monthsetting", "every", /month/i)
-    .addEvent("monthdayelaborating", "monthsetting", /(on)|(the)/i)
-    .addEvent("monthdayelaboratedset", "monthdayelaborating", /[123]?((1st)|(2nd)|(3rd)|([4-9]th))/i)
+    .addEvent("monthdayelaborating", ["monthsetting", "monthdayelaborating"], /(on)|(the)/i)
+    .addEvent("monthdayelaboratedset", "monthdayelaborating", /([123]?[1-9])((st)|(th))/i)
     .addEvent("monthdaylastdayset", ["monthdayelaborating"], /lastday/i)
     .addEvent("timesetting", ["dayset", "weekdayset", "monthdayelaboratedset"
       , "monthdaylastdayset"], /at/i)
     .addEvent("timeset", ["timesetting", "dayset", "weekdayset", "monthdayelaboratedset"
       , "monthdaylastdayset"], /([01]?[0-9]):([01-5][0-9])(am|pm)/i)
+    .finalStates("dayset", "weekdayset", "monthdayelaboratedset", "monthdaylastdayset", "timeset")
   .create();
 
   lexer.input = input;
   lexer.lex();
 }
+
+// parse("every day");
+// parse("every month on the 15th");
+// parse("every monday at 12:00pm");
